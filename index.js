d6e462fa73d2687681eb36f3a5798ba1b0fef1a4
dwcalade <dwcalade@yahoo.ca>
2022-04-15 06:35:57 +0000

    Iss_spotterIndex

---
+++ index.js
@@ -0,0 +1,41 @@
+/**
+ * Makes a single API request to retrieve the user's IP address.
+ * Input:
+ *   - A callback (to pass back an error or the IP string)
+ * Returns (via Callback):
+ *   - An error, if any (nullable)
+ *   - The IP address as a string (null if error). Example: "162.245.144.188"
+ */
+ const request = require('request');
+ const fetchMyIp = require('./iss');
+ const { nextISSTimesForMyLocation } = require('./iss_promised');
+
+  const printPassTimes = function(passTimes) {
+    for (const pass of passTimes) {
+      const datetime = new Date(0);
+      datetime.setUTCSeconds(pass.risetime);
+      const duration = pass.duration;
+      console.log(`Next pass at ${datetime} for ${duration} seconds!`);
+    }
+  };
+  
+  nextISSTimesForMyLocation((error, passTimes) => {
+    if (error) {
+      return console.log("It didn't work!", error);
+    }
+    // success, print out the deets!
+    printPassTimes(passTimes);
+  });
+  
+
+nextISSTimesForMyLocation()
+  .then((passTimes) => {
+    printPassTimes(passTimes);
+  })
+  .catch((error) => {
+    console.log("It didn't work: ", error.message);
+  });
+  
+
+  
+  module.exports = { fetchMyIP };
